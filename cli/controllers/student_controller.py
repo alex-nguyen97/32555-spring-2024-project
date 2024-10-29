175091065747e6bf6392b87e772978605f45a3ef
karishmak1311 <karishma.kumar@student.uts.edu.au>
2024-10-29 01:59:15 +0000

    Table & Message

--- cli/controllers/student_controller.py
+++ cli/controllers/student_controller.py
@@ -2,6 +2,7 @@ from models.student import Student
 from models.database import Database
 from colors.text_colors import *
 import re
+from tabulate import tabulate
 from ..utils.utils import Utils
 import os
 from dotenv import load_dotenv
@@ -73,18 +74,20 @@ class StudentController:
             if not re.match(Utils.EMAIL_REGEX, email) or not re.match(Utils.PASSWORD_REGEX, password):
                 print(RED + "Incorrect email or password format." + RESET)
             else:
-                print(YELLOW + "Successfully Logged In." + RESET)
+
                 break
 
         for student in self.student_list:
             if student.email == email and student.password == password:
                 # If found, display login success message
+                print(GREEN + "Successfully logged in!" + RESET)
                 self.student_course_menu(student)
                 return student
 
-        # If not found, display error message
-        print(RED + "Student does not exist" + RESET)
+        print(RED + "Incorrect email or password. Please try again." + RESET)
         return None
+    
+        
 
     def check_student_exists_by_email(self, email):
         # Check if student exists in the database
@@ -112,15 +115,25 @@ class StudentController:
 
             if option == "c":
                 student.change_password()
-                # CHANGE MADE: Added success message after password change
+                
                 print(GREEN + "Password changed successfully." + RESET)
             elif option == "e":
                 student.enrol_subject()
             elif option == "r":
                 student.drop_subject()
             elif option == "s":
-                student.show_enrolled_subjects()
+                self.show_enrolled_subjects(student)
             elif option == "x":
                 break
             else:
                 ErrorMessageHandling.printInvalidEntry()
+
+    def show_enrolled_subjects(self, student):
+        
+        subjects = student.subjects
+        if not subjects:
+            print(RED + "No subjects enrolled." + RESET)
+        else:
+            table_data = [[subject.ID, subject.mark, subject.grade] for subject in subjects]
+            headers = ["Subject ID", "Mark", "Grade"]
+            print(tabulate(table_data, headers, tablefmt="fancy_grid"))
